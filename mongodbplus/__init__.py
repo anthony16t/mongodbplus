55b045e362e27043d1447840a32e409104a8af4b
Anthony Toribio <anthonytoribio@anthonys-mbp.myfiosgateway.com>
2021-11-26 12:08:17 -0500

    added schema and added ingrement id

--- mongodbplus/__init__.py
+++ mongodbplus/__init__.py
@@ -1,14 +1,18 @@
-from pymongo import MongoClient
+from pymongo import MongoClient as CLIENT
 import json,os
-
 class Colors: fail = '\033[91m' ; good = '\033[92m' ; end = '\033[0m'
 class MongodbPlus:
     def __init__(self,databaseName:str):
         self.thisDir = os.path.dirname(__file__)+'/'
         self.databaseName=databaseName
-        self.__mongoClient = MongoClient("mongodb://localhost:27017/")
+        self.__mongoClient = CLIENT("mongodb://localhost:27017/")
         self.__mongoDb = self.__mongoClient[databaseName]
         self.__wasCollectionSet__ = False
+        defaultPath = f'{self.thisDir}__defaults__.json'
+        # create default json file to store db information
+        if not os.path.exists(defaultPath):
+            open(defaultPath,'w').write('{}')
+        self.defaultPath=defaultPath
 
     def setCollection(self,collectionName:str):
         self.collection = self.__mongoDb[collectionName]
@@ -82,17 +86,64 @@ class MongodbPlus:
         result = col.update_many(filter,{'$set':newData})
         return result
 
-    def insertOne(self,document:dict={},collectionName:str='',checkDuplicate:bool=False,duplicateKey:str=''):
+    def insertOne(self,document:dict={},collectionName:str='',checkDuplicate:bool=False,duplicateKey:str='',checkSchema=False):
         # check if collection name was setted
         if collectionName: collectionName=collectionName
         elif self.__wasCollectionSet__: collectionName=self.collectionName
         else: self.__printCollectionNameError__() ; return False
         col = self.__mongoDb[collectionName]
+        # get defaults info, if schema and lastId not found create a new one
+        default = json.loads(open(self.defaultPath,'r').read())
+        if collectionName not in default:
+            collectionSchema={}
+            for docName,docValue in document.items():
+                schemaType = type(docValue).__name__
+                collectionSchema[docName]=schemaType
+            default[collectionName]={
+                'lastId':0,
+                'schema':collectionSchema
+            }
+        # if checking for collection schema
+        if checkSchema:
+            schema = default[collectionName]['schema']
+            _document = document
+            schemaLength = len(schema)
+            docLength = len(document)
+            if '_id' in document: docLength=docLength-1 ; del _document['_id']
+            if schemaLength != docLength:
+                print(f'{Colors.fail}Schema size supposed to be {schemaLength} and the document size is {docLength}{Colors.end}')
+                print(f'{Colors.fail}Document size supposed to be {docLength} and the document size is {docLength}{Colors.end}')
+                print(f'{Colors.fail}Schema: {schema}{Colors.end}')
+                print(f'{Colors.fail}Document: {_document}{Colors.end}')
+                return False
+            # check for schema keys
+            for Key,KeyValue in _document.items():
+                if Key not in schema:
+                    print(f'{Colors.fail}Key: "{Key}" not found in collection: "{collectionName}" schema{Colors.end}')
+                    print(f'{Colors.fail}Schema: {schema}{Colors.end}')
+                    print(f'{Colors.fail}Update schema file __defaults__.json or use schema keys and types: {schema}{Colors.end}')
+                    print(f'{Colors.fail}Path: __defaults__.json = {self.thisDir}__defaults__.json{Colors.end}')
+                    return False
+                else:
+                    schemaKeyValueType = schema[Key]
+                    givenKeyValueType = type(KeyValue).__name__
+                    if schemaKeyValueType != givenKeyValueType:
+                        print(f'{Colors.fail}Key: "{Key}" expecting a "{schemaKeyValueType}" not "{givenKeyValueType}"{Colors.end}')
+                        print(f'{Colors.fail}Schema: {schema}{Colors.end}')
+                        print(f'{Colors.fail}Document: {_document}{Colors.end}')
+                        return False
         # check if _id was given if yes check if it exists
+        newId = default[collectionName]['lastId']+1
+        # check if new id exists, if yes get a new one
+        while True:
+            if not col.find_one({'_id':newId}): break
+            else: newId=newId+1
+        # check if _id was given on document
         if '_id' in document:
-            _id=document['_id']
-            if col.find_one({'_id':_id}):
-                print(f'{Colors.fail}Document with _id: {_id} already exists when trying to insert {self.cuteME(document)}{Colors.end}') ; return False
+            givenId=document['_id']
+            if col.find_one({'_id':givenId}):
+                print(f'{Colors.fail}Document with _id: {givenId} already exists when trying to insert {self.cuteME(document)}{Colors.end}') ; return False
+        else:document['_id']=newId
         # if check duplicate is set to true
         if checkDuplicate:
             if not duplicateKey:
@@ -106,21 +157,71 @@ class MongodbPlus:
                 print(f'{Colors.fail}Key: {keyName} with value: {keyValue} already exists{Colors.end}') ; return False
         # else insert
         result = col.insert_one(document)
+        # update defaults
+        default[collectionName]['lastId']=newId
+        open(self.defaultPath,'w').write(json.dumps(default,indent=4))
         return result
 
-    def insertMany(self,listOfDocuments:list=[],collectionName:str='',checkDuplicate:bool=False,duplicateKey:str=''):
+    def insertMany(self,listOfDocuments:list=[],collectionName:str='',checkDuplicate:bool=False,duplicateKey:str='',checkSchema:bool=False):
         # check if collection name was setted
         if collectionName: collectionName=collectionName
         elif self.__wasCollectionSet__: collectionName=self.collectionName
         else: self.__printCollectionNameError__() ; return False
         col = self.__mongoDb[collectionName]
-        newDocumentData=[]
+        idsInserted=[]
         for document in listOfDocuments:
+            # get defaults info, if schema and lastId not found create a new one
+            default = json.loads(open(self.defaultPath,'r').read())
+            if collectionName not in default:
+                collectionSchema={}
+                for docName,docValue in document.items():
+                    schemaType = type(docValue).__name__
+                    collectionSchema[docName]=schemaType
+                default[collectionName]={
+                    'lastId':0,
+                    'schema':collectionSchema
+                }
+            # if checking for collection schema
+            if checkSchema:
+                schema = default[collectionName]['schema']
+                _document = document
+                schemaLength = len(schema)
+                docLength = len(document)
+                if '_id' in document: docLength=docLength-1 ; del _document['_id']
+                if schemaLength != docLength:
+                    print(f'{Colors.fail}Schema size supposed to be {schemaLength} and the document size is {docLength}{Colors.end}')
+                    print(f'{Colors.fail}Document size supposed to be {docLength} and the document size is {docLength}{Colors.end}')
+                    print(f'{Colors.fail}Schema: {schema}{Colors.end}')
+                    print(f'{Colors.fail}Document: {_document}{Colors.end}')
+                    return False
+                # check for schema keys
+                for Key,KeyValue in _document.items():
+                    if Key not in schema:
+                        print(f'{Colors.fail}Key: "{Key}" not found in collection: "{collectionName}" schema{Colors.end}')
+                        print(f'{Colors.fail}Schema: {schema}{Colors.end}')
+                        print(f'{Colors.fail}Update schema file __defaults__.json or use schema keys and types: {schema}{Colors.end}')
+                        print(f'{Colors.fail}Path: __defaults__.json = {self.thisDir}__defaults__.json{Colors.end}')
+                        return False
+                    else:
+                        schemaKeyValueType = schema[Key]
+                        givenKeyValueType = type(KeyValue).__name__
+                        if schemaKeyValueType != givenKeyValueType:
+                            print(f'{Colors.fail}Key: "{Key}" expecting a "{schemaKeyValueType}" not "{givenKeyValueType}"{Colors.end}')
+                            print(f'{Colors.fail}Schema: {schema}{Colors.end}')
+                            print(f'{Colors.fail}Document: {_document}{Colors.end}')
+                            return False
             # check if _id was given if yes check if it exists
+            newId = default[collectionName]['lastId']+1
+            # check if new id exists, if yes get a new one
+            while True:
+                if not col.find_one({'_id':newId}): break
+                else: newId=newId+1
+            # check if _id was given on document
             if '_id' in document:
-                _id=document['_id']
-                if col.find_one({'_id':_id}):
-                    print(f'{Colors.fail}Document with _id: {_id} already exists when trying to insert {self.cuteME(document)}{Colors.end}') ; return False
+                givenId=document['_id']
+                if col.find_one({'_id':givenId}):
+                    print(f'{Colors.fail}Document with _id: {givenId} already exists when trying to insert {self.cuteME(document)}{Colors.end}') ; return False
+            else:document['_id']=newId
             # if check duplicate is set to true
             if checkDuplicate:
                 if not duplicateKey:
@@ -132,9 +233,13 @@ class MongodbPlus:
                 if col.find_one({duplicateKey:document[duplicateKey]}):
                     keyName,keyValue=duplicateKey,document[duplicateKey]
                     print(f'{Colors.fail}Key: {keyName} with value: {keyValue} already exists{Colors.end}') ; return False
-            newDocumentData.append(document)
-        result = col.insert_many(newDocumentData)
-        return result
+            # insert document
+            col.insert_one(document)
+            idsInserted.append(newId)
+            # update defaults
+            default[collectionName]['lastId']=newId
+            open(self.defaultPath,'w').write(json.dumps(default,indent=4))
+        return {'idsInserted':idsInserted}
 
     def drop(self,collectionName:str=''):
         # check if collection name was setted
